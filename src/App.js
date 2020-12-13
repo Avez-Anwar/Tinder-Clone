89e2f64d4c1e602c62d535254aed03e8a6dbe22b
Avez-Anwar <34218292+Avez-Anwar@users.noreply.github.com>
2020-12-13 16:01:01 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Header from "./Header";
+import SwipeButtons from "./SwipeButtons";
+import TinderCards from "./TinderCards";
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <TinderCards />
+      <SwipeButtons />
     </div>
   );
 }
